2bb17bd90b59b87d5a5b56dfcf324f8f5a5a4242
Federico <piruloequis@gmail.com>
2021-07-15 14:25:01 +0200

    add snaps animation and focus

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,38 +1,80 @@
 const intro = document.querySelector('.intro'),
+      screen = document.querySelector('.screen'),
       innerLens = document.querySelector('.lens-inner'),
       outerLens = document.querySelector('.lens-outer'),
-      focClick = new Audio('./assets/sound/camClick.mp3'),
+      focClick = new Audio('./assets/sound/camClick2.mp3'),
+      snapsMe = document.querySelectorAll('.snap-pic'),
       goBtn = document.querySelector('.intro-btn');
 
 //adding event listeners
 goBtn.addEventListener('click', function(){
+
+    focClick.play();
     focusLens();
+    focusMe();
+    setTimeout(function(){takeSnaps();}, 800);
+    
+
 });
 
-//adding functions for focusein the lens
+//adding functions for focusing the lens
 function focusLens() {
     //console.log('focuses');
     setTimeout(function(){
         reticle(outerLens, '60deg');
-        reticle(innerLens, '-60deg', true);
+        reticle(innerLens, '-60deg', true, "0.7");
      }, 100);
 
      setTimeout(function(){
-        reticle(outerLens, '-10deg');
-        reticle(innerLens, '10deg', true);
-     }, 700);
-
-    focClick.play();
+        reticle(outerLens, '-20deg');
+        reticle(innerLens, '20deg', true, "0.85");
+     }, 600);
     
 }
 
-function reticle(elem, angle, close = false){
+function reticle(elem, angle, close = false, scale = 1){
     //console.log('focuses', elem);
     elem.style.transform = "rotate(" + angle + ")";
 
     if(close) {
-        elem.style.transform = "scale(0.80)";
+        elem.style.transform = "scale(" + scale + ")";
     }
 
-    elem.style.transition = "ease-in-out 500ms";
+    elem.style.transition = "ease-in 500ms";
+}
+
+//bringing first image to focus
+function focusMe(){
+    console.log('on focus');
+    
+    //first focus step
+    setTimeout(function(){focusElem(snapsMe[0], "300");}, 100);
+    setTimeout(function(){focusElem(snapsMe[0], "200", "4px");}, 400);
+    setTimeout(function(){focusElem(snapsMe[0], "500");}, 700);
+}
+
+function focusElem(elem, delay = 0, shift = 0){
+    elem.style.filter = "blur(" + shift + ")";
+    elem.style.transition = "ease-in " + delay + "ms";
+}
+
+//taking the snapshots
+function takeSnaps(){
+
+    //loping through the snaps
+    for (let i = 1; i < 4; i++) {
+        
+        //console.log(snapsMe[i]);
+        setTimeout(function(){
+            toggle(snapsMe[i - 1]);
+            toggle(snapsMe[i]);
+            screen.style.backgroundColor = "rgba(174, 209, 245, 0.4)";
+            setTimeout(function() {
+                screen.style.backgroundColor = "transparent";
+            }, 100);
+
+        }, 600 * i);
+
+        
+    }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@
 
 console.log('hello');
 
+//miscellanoeus functions 
+function toggle(elem){
+    elem.classList.toggle('hidden');
+}
+
 
 window.onload = function(){
     console.log('loaded');
